b02314b55e47113b8f5b6edbc692edcd417aeecc
vitoliu <vitoliu@xiaoman.cn>
2021-08-03 23:25:44 +0800

    1

--- 草稿中/xm/quickSort.ts
+++ 草稿中/xm/quickSort.ts
@@ -37,14 +37,64 @@ const quickSort = <T>(
   return arr
 }
 
-console.log(...quickSort([5, 4, 3, 2, 1]))
-console.log(...quickSort([5, 2, 3, 6, 1]))
-console.log(...quickSort([]))
-console.log(...quickSort([1]))
-console.log(...quickSort([1, 2, 3]))
-console.log(...quickSort([8, 9, 10, 40, 2, 3, 40, 40, 40]))
-console.log(...quickSort([1, 1, 1, 1, 1]))
-console.log(...quickSort([8, 9, 9, 8]))
-console.log(...quickSort([8, 9, 9, 9, 8]))
-console.log(...quickSort([8, 8, 9, 8, 8]))
-console.log(...quickSort([20, 10, 6, 6, 7, 8, 20, 16]))
+// console.log(...quickSort([5, 4, 3, 2, 1]))
+// console.log(...quickSort([5, 2, 3, 6, 1]))
+// console.log(...quickSort([]))
+// console.log(...quickSort([1]))
+// console.log(...quickSort([1, 2, 3]))
+// console.log(...quickSort([8, 9, 10, 40, 2, 3, 40, 40, 40]))
+// console.log(...quickSort([1, 1, 1, 1, 1]))
+// console.log(...quickSort([8, 9, 9, 8]))
+// console.log(...quickSort([8, 9, 9, 9, 8]))
+// console.log(...quickSort([8, 8, 9, 8, 8]))
+// console.log(...quickSort([20, 10, 6, 6, 7, 8, 20, 16]))
+
+
+const quickSort2 = <T>(
+  arr: T[],
+  cmp = (beforeV: T, afterV: T) => beforeV <= afterV,
+  range = { s: 0, e: arr.length - 1  }
+) => {
+  if (range?.s < 0 || range?.e > arr.length - 1 || range?.s >= range?.e || arr.length <= 1) {
+    return arr
+  }
+
+  const t = arr[range.s]
+
+  let s = range?.s
+  let e = range?.e
+
+  while (s < e) {
+    while(s < e && cmp(t, arr[e])) {
+      e--
+    }
+    if (s < e) {
+      arr[s] = arr[e]
+    }
+
+    while(s < e && cmp(arr[s], t)) {
+      s++
+    }
+    if (s < e) {
+      arr[e] = arr[s]
+    }
+  }
+
+  arr[s] = t
+  quickSort2(arr, cmp, { s: range.s, e: s - 1 })
+  quickSort2(arr, cmp, { s: s + 1 , e: range.e })
+  return arr
+}
+
+
+console.log(...quickSort2([5, 4, 3, 2, 1]))
+console.log(...quickSort2([5, 2, 3, 6, 1]))
+console.log(...quickSort2([]))
+console.log(...quickSort2([1]))
+console.log(...quickSort2([1, 2, 3]))
+console.log(...quickSort2([8, 9, 10, 40, 2, 3, 40, 40, 40]))
+console.log(...quickSort2([1, 1, 1, 1, 1]))
+console.log(...quickSort2([8, 9, 9, 8]))
+console.log(...quickSort2([8, 9, 9, 9, 8]))
+console.log(...quickSort2([8, 8, 9, 8, 8]))
+console.log(...quickSort2([20, 10, 6, 6, 7, 8, 20, 16]))
--- 草稿中/xm/tow.ts
+++ 草稿中/xm/tow.ts
@@ -3,18 +3,120 @@ const binarySearch = (arr: number[], target: number) => {
     return -1
   }
 
-  let r = 0
-  let l = arr.length - 1
-  while (r <= l) {
-    const mid = Math.floor((r + l) / 2)
+  let left = 0
+  let right = arr.length - 1
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2)
     if (arr[mid] === target) {
       return mid
     } else if (arr[mid] > target) {
-      l = mid - 1
+      right = mid - 1
     } else {
-      r = mid + 1
+      left = mid + 1
     }
   }
 
   return -1
 }
+
+
+const binarySearch2 = (arr: number[], target: number) => {
+  if (!arr || !arr.length) {
+    return -1
+  }
+
+  let l = 0
+  let r = arr.length - 1
+
+  while(l <= r) {
+    const mid = Math.floor((l + r) / 2)
+    if (arr[mid] === target) {
+      return mid
+    }
+    if (arr[mid] < target) {
+      l = mid + 1
+    } else {
+      r = mid - 1
+    }
+  }
+
+  console.log(l, r)
+  return -1
+}
+
+console.log('单独')
+binarySearch2([5], 5)
+
+console.log('前面加数字')
+binarySearch2([4,5], 5)
+binarySearch2([2,4,5], 5)
+
+console.log('后面加数字')
+binarySearch2([5,6], 5)
+binarySearch2([5,6,7], 5)
+
+console.log('前后多')
+binarySearch2([4,5,6], 5)
+binarySearch2([3,4,5,6,7], 5)
+binarySearch2([2,3,4,5,6,7,8], 5)
+
+console.log('前1后多')
+binarySearch2([4,5,6,7], 5)
+binarySearch2([4,5,6,7,8], 5)
+
+console.log('后1前多')
+binarySearch2([3,4,5,6], 5)
+binarySearch2([2,3,4,5,6], 5)
+
+
+
+
+console.log('4.5 单独')
+binarySearch2([5], 4.5)
+
+console.log('4.5 前面加数字')
+binarySearch2([4,5], 4.5)
+binarySearch2([2,4,5], 4.5)
+
+console.log('4.5 后面加数字')
+binarySearch2([5,6], 4.5)
+binarySearch2([5,6,7], 4.5)
+
+console.log('4.5 前后多')
+binarySearch2([4,5,6], 4.5)
+binarySearch2([3,4,5,6,7], 4.5)
+binarySearch2([2,3,4,5,6,7,8], 4.5)
+
+console.log('4.5 前1后多')
+binarySearch2([4,5,6,7], 4.5)
+binarySearch2([4,5,6,7,8], 4.5)
+
+console.log('4.5 后1前多')
+binarySearch2([3,4,5,6], 4.5)
+binarySearch2([2,3,4,5,6], 4.5)
+
+
+
+console.log('5.5 单独')
+binarySearch2([5], 5.5)
+
+console.log('5.5 前面加数字')
+binarySearch2([4,5], 5.5)
+binarySearch2([2,4,5], 5.5)
+
+console.log('5.5 后面加数字')
+binarySearch2([5,6], 5.5)
+binarySearch2([5,6,7], 5.5)
+
+console.log('5.5 前后多')
+binarySearch2([4,5,6], 5.5)
+binarySearch2([3,4,5,6,7], 5.5)
+binarySearch2([2,3,4,5,6,7,8], 5.5)
+
+console.log('5.5 前1后多')
+binarySearch2([4,5,6,7], 5.5)
+binarySearch2([4,5,6,7,8], 5.5)
+
+console.log('5.5 后1前多')
+binarySearch2([3,4,5,6], 5.5)
+binarySearch2([2,3,4,5,6], 5.5)
